33376353ed0efa41968d6245ad1ed3ad096b1b00
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-11-10 19:03:41 +0100

    Traccia js-simon completata
    Traccia js-simon finita

--- main.js
+++ main.js
@@ -1,43 +1,89 @@
-//Visualizzare in pagina 5 numeri casuali
+////Visualizzare in pagina 5 numeri casuali
+const numeriCasuali = getRandomNumbers(5, 1, 100);
 
-// Funzione per generare HTML con un numero casuale
-function generaHTML(indice) {
-    let numeroRandom = numeroCasuale(1, 100);
-    return "Il numero " + indice + " è: " + numeroRandom;
-}
+//////stampo l'array in pagina 
+document.getElementById("numeroInPagina").innerHTML = numeriCasuali.join(", ");
+
+////dopo 3s, faccio scompare i numeri scompaiono dal DOM
+setTimeout(function() {
+    document.getElementById("numeroInPagina").innerHTML = "";
+}, 3000);
+
+////dopo 3s chiedo i numeri all'utente
+setTimeout (function() {
+    let numeriUtente =  numeroUsuario ();
+    let numeriInduvinatiUtente = numeriInduvinati(numeriCasuali, numeriUtente);
+    alert("Hai indovinato " + numeriInduvinatiUtente.length + " numeri");
+    alert("I numeri che hai indovinato sono: " + numeriInduvinatiUtente.join(", ") );
+    alert("I numeri giusti erano: " + numeriCasuali.join (", "))
+}, 3100);
 
 
+///////////////////// FUNZIONI//////////////////////////
 
+// Funzione per sapere quanti numeri sono stati indovinati
+function numeriInduvinati(arrayList, arrayConfronto){
+    // array vuoto per contenere i numeri indovinati
+    let risultato = []; 
 
-// Imposta l'HTML degli elementi
-const numero1 =document.getElementById("numeroInPagina1").innerHTML = generaHTML(1);
-const numero2 =document.getElementById("numeroInPagina2").innerHTML = generaHTML(2);
-const numero3 =document.getElementById("numeroInPagina3").innerHTML = generaHTML(3);
-const numero4 =document.getElementById("numeroInPagina4").innerHTML = generaHTML(4);
-const numero5 =document.getElementById("numeroInPagina5").innerHTML = generaHTML(5);
-
-let contatore = 0; 
-// Nascondi l'HTML dopo 30 secondi
-setInterval(function() {
-    contatore++;
-    nascondiContenuto();
-}, 30000);
-
-// Funzione per nascondere il contenuto
-function nascondiContenuto() {
+    for(let i = 0; i <arrayList.length; i++){
+        if( arrayConfronto.includes(arrayList[i])) {
+        // se al confrontare i dui nummeri sono uguali, allora aggiunge elemento a risultato
+        risultato.push(arrayList[i]);
+        
+    }
     
-    document.getElementById("numeroInPagina1").style.display = "none";
-    document.getElementById("numeroInPagina2").style.display = "none";
-    document.getElementById("numeroInPagina3").style.display = "none";
-    document.getElementById("numeroInPagina4").style.display = "none";
-    document.getElementById("numeroInPagina5").style.display = "none";
+}
+// restituisce  l'array contenenet i numeri indovinati
+return risultato;
 }
 
-const numeroUtente = Number(prompt("Inserisci un numero"));
+
+// l'utente inserisce 5 numeri da prompt 
+function numeroUsuario(){
+    //Dichiaramo un array vuro per inserire i numeri che l'utente fornisce con il prompt
+    let risultato = [];
+    //  il cliclo non finisce fino che " risultato" non contiene 5 numeri 
+    while(risultato.length <5){
+
+        // chiedo all'utente un numero 
+        let nuevoNumero = parseInt(prompt("Inserisce un numero")); 
+
+        // // Se il numero non è presente, lo aggiungo all'array 'risultato'.
+        if (!risultato.includes(nuevoNumero)){
+            risultato.push(nuevoNumero);
+        }
+        // Se il numero è già presente, mostro un messaggio di avviso all'utente.
+        else{
+            alert ("Non puoi inserire una numero già inserito")
+        }
+    }
+    // Una volta ottenuti 5 numeri, ristituisce l'array "risultato"
+    return risultato;
+}
+
+
+//Funzione che contendra numeri causuali e controllerà che tutti siano diversi tra loro
+function getRandomNumbers(quantiNumeri, min, max) {
+    let risultato = [];
+
+    // cliclo while che repite funche non sia raggiunto la quantita di numeri desiderari
+    while (risultato.length < quantiNumeri) {
+        // genera un nuovo numero casuale
+        const nuevoNumero = numeroCasuale(min, max);
+        // controlla che non sia nel array 
+        if (!risultato.includes(nuevoNumero)) {
+             // se non è nel array lo pusha
+            risultato.push(nuevoNumero);
+            console.log(nuevoNumero)
+        }
+       
+    }
+    return risultato;
+}
+
 // Funzione per generare un numero casuale
 function numeroCasuale(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+    
 }
-
-
-
